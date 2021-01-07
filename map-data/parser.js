4fe57746f95f904b22fad840fe43d1e7c16e942c
Yoan Gauthier <yoangau@gmail.com>
2021-01-06 20:49:46 -0500

    add parser and raw data

---
+++ map-data/parser.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const _ = require("lodash")
+
+const rawCountryData = fs.readFileSync("./populated-places-simple-50m.json")
+const rawFlagsData = fs.readFileSync("./flag-emojis-by-code.json")
+
+const countries = JSON.parse(rawCountryData).features
+const flagsData = JSON.parse(rawFlagsData)
+
+const countriesWithFlags = _.transform(countries, (acc, curr) => {
+    const prop = curr.properties
+    acc.push({
+        city: prop.name,
+        country: prop.adm0name,
+        adminName: prop.adm1name,
+        flag: flagsData[prop.iso_a2]?.emoji,
+        isCapital: prop.adm0cap === 1,
+        coordinates: curr.geometry.coordinates
+    })
+}, [])
+
+fs.writeFileSync("./cities-flags.json", JSON.stringify(countriesWithFlags))
+
+process.exit()
