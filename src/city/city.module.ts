6f9f5023c5f4e8660094cb6b064f1b22879d0457
Yoan Gauthier <yoangau@gmail.com>
2021-01-08 16:52:05 -0500

    remove city controller everywhere

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { City } from './city.entity';
 import { CityService } from './city.service';
-import { CityController } from './city.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([City])],
   providers: [CityService],
   exports: [CityService],
-  controllers: [CityController],
 })
 export class CityModule {}
