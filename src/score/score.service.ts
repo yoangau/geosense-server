de3bba8031302aeae8b6496bbc637a76b58cc179
Yoan Gauthier <yoangau@gmail.com>
2021-01-08 16:52:19 -0500

    add repo to score service

--- src/score/score.service.ts
+++ src/score/score.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Score } from './score.entity';
 
 @Injectable()
-export class ScoreService {}
+export class ScoreService {
+  constructor(
+    @InjectRepository(Score)
+    private cityRepository: Repository<Score>,
+  ) {}
+}
