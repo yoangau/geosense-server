b875664c18271dc84e97588541c98d6ccb551bf2
Yoan Gauthier <yoangau@gmail.com>
2021-01-09 17:56:10 -0500

    fix import

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { GameDTO } from './game.dto';
 import { Game } from './game.entity';
 import { GameService } from './game.service';
