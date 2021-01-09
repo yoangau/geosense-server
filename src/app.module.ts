c825e59695b13e9fc6c411374e24bf028a658c63
Yoan Gauthier <yoangau@gmail.com>
2021-01-09 17:36:01 -0500

    init group module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { CityModule } from './city/city.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { ScoreModule } from './score/score.module';
+import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [GameModule, CityModule, UserModule, ScoreModule, TypeOrmModule.forRoot()],
+  imports: [GameModule, CityModule, UserModule, ScoreModule, TypeOrmModule.forRoot(), GroupModule],
   controllers: [],
   providers: [],
 })
