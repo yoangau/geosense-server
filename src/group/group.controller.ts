d9afcdee7c015d45b83d565a5c25fa943d3961dc
Yoan Gauthier <yoangau@gmail.com>
2021-03-19 11:55:24 -0400

    create basic group controller

--- src/group/group.controller.ts
+++ src/group/group.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
+import { Group } from './group.entity';
+import { GroupService } from './group.service';
 
 @Controller('group')
-export class GroupController {}
+export class GroupController {
+  constructor(private groupService: GroupService) {}
+
+  @Post(':id')
+  addOne(@Param('id') id: string): Promise<Group> {
+    return this.addOne(id);
+  }
+}
