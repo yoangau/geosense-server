87c32b7e13bd371a020b3ac88e3617b8e16c3b2b
Yoan Gauthier <yoangau@gmail.com>
2021-03-19 11:56:26 -0400

    add service methods

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserService } from 'src/user/user.service';
 import { Repository } from 'typeorm';
+import { GroupUserDTO } from './group.dto';
 import { Group } from './group.entity';
 
 @Injectable()
@@ -18,6 +19,17 @@ export class GroupService {
 
   async addOne(adminId: string): Promise<Group> {
     const user = await this.userService.getOne(adminId);
-    return this.groupRepository.save({ user, users: [user], dateCreated: new Date() });
+    return this.groupRepository.save({ admin: user, users: [user], dateCreated: new Date() });
+  }
+
+  async addUser({ userId, groupId }: GroupUserDTO) {
+    const user = await this.userService.getOne(userId);
+    const group = await this.findOne(groupId);
+    return this.groupRepository.save({ ...group, users: [...group.users, user] });
+  }
+
+  async removeUser({ userId, groupId }: GroupUserDTO) {
+    const group = await this.findOne(groupId);
+    return this.groupRepository.save({ ...group, users: group.users.filter(u => u.id !== userId) });
   }
 }
