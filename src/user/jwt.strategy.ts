b76e8caf5b8739712947a02659526b1c1fd1d903
Yoan Gauthier <yoangau@gmail.com>
2021-03-20 13:02:01 -0400

    remove console log in jtw strat

--- src/user/jwt.strategy.ts
+++ src/user/jwt.strategy.ts
@@ -15,7 +15,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JWTPayload) {
-    console.log(payload);
     return { userId: payload.userId };
   }
 }
