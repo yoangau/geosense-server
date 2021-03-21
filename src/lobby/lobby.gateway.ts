67e617da47926e92597a357a251a217e0f5e494b
Yoan Gauthier <yoangau@gmail.com>
2021-03-20 23:51:31 -0400

    add lobby pipe to lobby gateway

--- src/lobby/lobby.gateway.ts
+++ src/lobby/lobby.gateway.ts
@@ -1,7 +1,17 @@
-import { OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Body, Inject, UsePipes } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { LobbyUserAdminDTO, LobbyUserDTO } from './lobby.dto';
+import { LobbyPipeDTO } from './lobby.dto';
 import { LobbyEmitEvent, LobbySubEvent } from './lobby.events';
+import { LobbyPipe } from './lobby.pipe';
 import { LobbyService } from './lobby.service';
 
 @WebSocketGateway()
@@ -12,29 +22,28 @@ export class LobbyGateway implements OnGatewayDisconnect {
   @WebSocketServer()
   protected server: Server;
 
-  constructor(private lobbyService: LobbyService) {}
+  constructor(private moduleRef: ModuleRef, private lobbyService: LobbyService) {}
 
   handleDisconnect(client: Socket) {
     const userId = this.socketIdsToUserIds[client.id];
     delete this.userIdsToSockets[userId];
     delete this.socketIdsToUserIds[client.id];
+    Object.keys(client.rooms).forEach(lobbyId => this.lobbyService.removeUser({ userId, lobbyId }));
   }
 
   @SubscribeMessage(LobbySubEvent.Join)
-  async handleJoin(client: Socket, lobbyUser: LobbyUserDTO) {
-    const { userId, lobbyId } = lobbyUser;
-    const lobby = await this.lobbyService.addUser(lobbyUser);
-    client.join(lobbyId);
-    this.userIdsToSockets[userId] = client;
-    this.socketIdsToUserIds[client.id] = userId;
-    this.server.to(lobbyId).emit(LobbyEmitEvent.Update, lobby);
+  async handleJoin(@ConnectedSocket() client: Socket, @MessageBody(LobbyPipe) { user, lobby }: LobbyPipeDTO) {
+    lobby.users.push(user);
+    client.join(lobby.id);
+    this.userIdsToSockets[user.id] = client;
+    this.socketIdsToUserIds[client.id] = user.id;
+    this.server.to(lobby.id).emit(LobbyEmitEvent.Update, lobby);
   }
 
   @SubscribeMessage(LobbySubEvent.Remove)
-  async handleRemove(client: Socket, { adminId, ...lobbyUser }: LobbyUserAdminDTO) {
-    const { userId, lobbyId } = lobbyUser;
-    const lobby = await this.lobbyService.removeUser(lobbyUser);
-    this.userIdsToSockets[userId].leave(lobbyId);
-    this.server.to(lobbyId).emit(LobbyEmitEvent.Update, lobby);
+  async handleRemove(@MessageBody(LobbyPipe) { user, lobby }: LobbyPipeDTO) {
+    lobby.removeUser(user);
+    this.userIdsToSockets[user.id].leave(lobby.id);
+    this.server.to(lobby.id).emit(LobbyEmitEvent.Update, lobby);
   }
 }
