5a1636abb773d08d263009fa09bdba234e9ad883
Yoan Gauthier <yoangau@gmail.com>
2021-03-23 00:41:57 -0400

    add get lobby querry

--- src/lobby/lobby.controller.ts
+++ src/lobby/lobby.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, UnauthorizedException, Post } from '@nestjs/common';
+import { Body, Controller, UnauthorizedException, Post, Get, NotFoundException, Query } from '@nestjs/common';
 import Lobby from './lobby';
 import { LobbyService } from './lobby.service';
 import { UserIdDTO } from 'src/user/user.dto';
@@ -12,5 +12,12 @@ export class LobbyController {
     const lobby = this.lobbyService.createLobby(userId);
     if (!lobby) throw new UnauthorizedException();
     return lobby as Promise<Lobby>;
+  }
+
+  @Get()
+  getOne(@Query('lobbyId') lobbyId: string): Lobby {
+    const lobby = this.lobbyService.findLobby(lobbyId);
+    if (!lobby) throw new NotFoundException();
+    return lobby;
   }
 }
