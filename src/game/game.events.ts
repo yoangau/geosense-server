eed085faef6834f589f8cf32c6bfa12d47cbb722
Yoan Gauthier <yoangau@gmail.com>
2021-03-28 21:25:16 -0400

    add game events

---
+++ src/game/game.events.ts
@@ -0,0 +1,10 @@
+export enum GameSubEvent {
+  Create = 'create-game',
+  Score = 'score',
+}
+
+export enum GameEmitEvent {
+  StartRound = 'start-round',
+  EndRound = 'end-round',
+  EndGame = 'end-game',
+}
