858c98e7bca50fe33e488714117132140a3900ee
Yoan Gauthier <yoangau@gmail.com>
2021-04-04 18:37:33 -0400

    put game state into it's own file

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -8,47 +8,7 @@ import { UserService } from 'src/user/user.service';
 import { Repository } from 'typeorm';
 import { GameDTO } from './game.dto';
 import { Game } from './game.entity';
-
-interface GameStateCtorParams {
-  startGameEvent: CallableFunction;
-  endGameEvent: CallableFunction;
-  waitRoundEvent: CallableFunction;
-  playRoundEvent: CallableFunction;
-}
-
-class GameState {
-  roundNumber = -1;
-  roundUsers: string[] = [];
-  roundStartTime = Date.now();
-  state: 'wait' | 'play' = 'wait';
-
-  private startGameEvent: CallableFunction;
-  endGameEvent: CallableFunction;
-
-  private waitRoundEvent: CallableFunction;
-  private playRoundEvent: CallableFunction;
-
-  constructor({ startGameEvent, endGameEvent, waitRoundEvent, playRoundEvent }: GameStateCtorParams) {
-    this.startGameEvent = startGameEvent;
-    this.endGameEvent = endGameEvent;
-    this.waitRoundEvent = waitRoundEvent;
-    this.playRoundEvent = playRoundEvent;
-    this.startGameEvent();
-  }
-
-  nextRound() {
-    this.roundNumber++;
-    this.roundUsers = [];
-    this.state = 'wait';
-    this.roundStartTime = Date.now();
-    this.waitRoundEvent();
-  }
-
-  playRound() {
-    this.state = 'play';
-    this.playRoundEvent();
-  }
-}
+import { GameState, GameStateCtorParams } from './game.state';
 
 @Injectable()
 export class GameService {
