4163196eb1dd95b49b52f94084079f583dd0a045
Yoan Gauthier <yoangau@gmail.com>
2021-04-03 23:10:49 -0400

    add score logic

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -2,15 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { SchedulerRegistry } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CityService } from 'src/city/city.service';
+import { ScoreService } from 'src/score/score.service';
+import { User } from 'src/user/user.entity';
 import { UserService } from 'src/user/user.service';
 import { Repository } from 'typeorm';
 import { GameDTO } from './game.dto';
 import { Game } from './game.entity';
 
 class GameState {
-  roundNumber = 0;
+  roundNumber = -1;
   roundUsers: string[] = [];
+  roundStartTime = Date.now();
   state: 'wait' | 'play' = 'wait';
+
+  nextRound() {
+    this.roundNumber++;
+    this.roundUsers = [];
+    this.state = 'wait';
+    this.roundStartTime = Date.now();
+  }
 }
 
 @Injectable()
@@ -22,6 +32,7 @@ export class GameService {
     private gameRepository: Repository<Game>,
     private cityService: CityService,
     private userService: UserService,
+    private scoreService: ScoreService,
     private schedulerRegistry: SchedulerRegistry,
   ) {}
 
@@ -37,6 +48,7 @@ export class GameService {
 
   startGame(game: Game) {
     this.gameStates[game.id] = new GameState();
+    console.log('emit start-game');
     this.startRound(game);
   }
 
@@ -50,14 +62,13 @@ export class GameService {
     if (!gameState || gameState.roundNumber >= game.cities.length) {
       return this.endGame(game);
     }
-    console.log('emit round-starting');
-    gameState.state = 'wait';
+    console.log('emit wait-round');
+    gameState.nextRound();
 
     const waitTimeout = setTimeout(() => {
-      console.log('emit round-starting');
+      console.log('emit play-round');
       gameState.state = 'play';
       const playTimeout = setTimeout(() => {
-        this.gameStates[game.id].roundNumber++;
         this.startRound(game);
       }, 10000);
       this.schedulerRegistry.addTimeout(game.id, playTimeout);
@@ -67,7 +78,6 @@ export class GameService {
 
   nextRound(game: Game) {
     this.schedulerRegistry.deleteTimeout(game.id);
-    this.gameStates[game.id].roundNumber++;
     this.startRound(game);
   }
 
@@ -76,5 +86,13 @@ export class GameService {
       ...game,
       cities: game.cities.slice(0, gameSate.roundNumber),
     };
+  }
+
+  async score(game: Game, user: User, latitude: number, longitude: number) {
+    const gameState = this.gameStates[game.id];
+    if (!gameState || gameState.state === 'wait' || !gameState.roundUsers.find(uid => uid === user.id)) return;
+    const city = game.cities[gameState.roundNumber];
+    const deltaTimeSeconds = (Date.now() - gameState.roundStartTime) / 1000;
+    return this.scoreService.score(game, user, city, latitude, longitude, deltaTimeSeconds);
   }
 }
