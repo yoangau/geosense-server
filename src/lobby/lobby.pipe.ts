21ba9f692bf35ce12518dc706a8525b21283c563
Yoan Gauthier <yoangau@gmail.com>
2021-10-25 21:20:39 -0400

    add optionnal game to lobby pipe

--- src/lobby/lobby.pipe.ts
+++ src/lobby/lobby.pipe.ts
@@ -1,5 +1,6 @@
 import { ArgumentMetadata, Inject, Injectable, PipeTransform } from '@nestjs/common';
 import { WsException } from '@nestjs/websockets';
+import { GameService } from 'src/game/game.service';
 import { UserService } from 'src/user/user.service';
 import { LobbyAdminDTO } from './lobby.dto';
 import { LobbyService } from './lobby.service';
@@ -9,16 +10,17 @@ export class LobbyPipe implements PipeTransform {
   constructor(
     @Inject(LobbyService) private lobbyService: LobbyService,
     @Inject(UserService) private userService: UserService,
-  ) {}
+    @Inject(GameService) private gameService: GameService,
+  ) { }
 
-  async transform({ lobbyId, userId, adminId }: LobbyAdminDTO, metadata: ArgumentMetadata) {
+  async transform({ lobbyId, userId, adminId, currentGameId }: LobbyAdminDTO, metadata: ArgumentMetadata) {
     const lobby = this.lobbyService.findLobby(lobbyId);
     const user = await this.userService.findOne(userId);
-
+    const game = currentGameId ? await this.gameService.find(currentGameId) : currentGameId
     if (!lobby) throw new WsException('This lobby does not exist');
     if (!user) throw new WsException('This user does not exist');
     if (adminId && lobby.admin.id !== adminId) throw new WsException('This is not the lobby admin');
 
-    return { lobby, user };
+    return { lobby, user, game };
   }
 }
